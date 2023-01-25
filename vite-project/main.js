722c907f2acb1f65b55474100174c5292ca4c3b5
matheuscastro77 <matheustvcastro1997@gmail.com>
2023-01-25 19:21:22 -0300

    all projetc done

---
+++ vite-project/main.js
@@ -0,0 +1,98 @@
+import * as THREE from 'three';
+import "./style.css";
+import gsap from 'gsap';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+//Scene
+const scene = new THREE.Scene();
+
+
+//Sphere
+const geometry = new THREE.SphereGeometry(3, 64, 64);
+const material = new THREE.MeshStandardMaterial({
+  color: '#00ff83',
+  roughness: 0.5,
+})
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh)
+
+//Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight
+}
+
+//Light
+const light = new THREE.PointLight(0xffffff, 1, 100)
+light.position.set(0, 10, 10)
+light.intensity = 1.25
+scene.add(light)
+
+//Camera
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 20
+scene.add(camera)
+
+//Renderer
+const canvas = document.querySelector('.webgl')
+const renderer = new THREE.WebGL1Renderer({ canvas })
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(2)
+renderer.render(scene, camera)
+
+//Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+controls.enablePan = false // mover a esfera
+controls.enableZoom = false // zoom
+controls.autoRotate = true
+controls.autoRotateSpeed = 3
+
+//Resize
+window.addEventListener('resize', () => {
+  //Update Sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+  //Update Camera
+  camera.updateProjectionMatrix()
+  camera.aspect = sizes.width / sizes.height
+  renderer.setSize(sizes.width, sizes.height)
+})
+
+const loop = () => {
+  //mesh.position.x += 0.01
+  controls.update()
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop)
+}
+loop()
+
+//Timeline magic
+
+const tl = gsap.timeline({ defaults: { duration: 1 } })
+tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 })
+tl.fromTo('nav', { y: "-100%" }, { y: "0%" })
+tl.fromTo(".title", { opacity: 0 }, { opacity: 1 })
+
+//Mouse Animation Color
+let mouseDown = false
+let rgb = []
+window.addEventListener("mousedown", () => (mouseDown = true))
+window.addEventListener("mouseup", () => (mouseDown = false))
+
+window.addEventListener("mousemove", (e) => {
+  if (mouseDown) {
+    rbg = [
+      Math.round((e.pageX / sizes.width) * 255),
+      Math.round((e.pageY / sizes.height) * 255),
+      150,
+    ]
+    //Let's animate
+    let newColor = new THREE.Color(`rgb(${rgb.join(",")})`)
+    gsap.to(mesh.material.color, {
+      r: newColor.r,
+      g: newColor.g,
+      b: newColor.b,
+    })
+  }
+})
